81f5ebdbd883a9fbacc4e8f00edf32fc6ec05883
Nathan Fischer <nathan@nathanfischer.dev.br>
2024-01-09 09:34:00 -0800

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ description }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+
+  data() {
+    return {
+      description: ''
+    }
+  }
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,80 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
+import router from '@/router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+const localVue = createLocalVue()
+
+describe('All tests', () => {
+  it('Renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
+
     expect(wrapper.text()).toMatch(msg)
+  })
+
+  it('HelloWorld rendered correctly', () => {
+    const wrapper = shallowMount(HelloWorld)
+
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('Snapshot HelloWorld', () => {
+    const wrapper = mount(HelloWorld)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('HelloWorld renders with the correct class', () => {
+    const wrapper = mount(HelloWorld)
+
+    expect(wrapper.classes()).toContain('hello')
+  })
+
+  it('Click on first button', async () => {
+    const wrapper = mount(HelloWorld)
+    await wrapper.find('#button').trigger('click')
+
+    expect(wrapper.vm.functionCalled).toBe(true)
+  })
+
+  it('Emits an custom event when second button is clicked', async () => {
+    const wrapper = mount(HelloWorld)
+    await wrapper.find('#buttonEvent').trigger('click')
+
+    expect(wrapper.emitted().buttonClick).toBeTruthy()
+  })
+
+  it('Renders the correct button text', () => {
+    const wrapper = mount(HelloWorld)
+
+    expect(wrapper.find('#buttonEvent').text()).toBe('Custom Event')
+  })
+
+  it('Check if renders two buttons', () => {
+    const wrapper = mount(HelloWorld)
+    const buttons = wrapper.findAll('button')
+
+    expect(buttons.length).toBe(2)
+  })
+
+  it('Route navigation occurs correctly', async () => {
+    const wrapper = mount(HomeView, {
+      localVue,
+      router
+    })
+    await wrapper.vm.$router.push('/about')
+
+    expect(wrapper.vm.$route.path).toBe('/about')
+  })
+
+  it('Description is changed correctly', async () => {
+    const wrapper = mount(AboutView)
+    await wrapper.setData({ description: 'This is an about page' })
+
+    expect(wrapper.vm.description).toBe('This is an about page')
   })
 })
